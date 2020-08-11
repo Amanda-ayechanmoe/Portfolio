3e8c0f794664f47242ba5a84605c9b4c9e726ec4
0eurEk4 <scriptkiddy.0eurek4@gmail.com>
2020-08-11 19:03:07 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { HomeComponent } from "./components/home/home.component";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "contact", component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
